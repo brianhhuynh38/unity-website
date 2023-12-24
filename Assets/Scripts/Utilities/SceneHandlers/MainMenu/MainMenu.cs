aa82614d3ab36a8d19cdc7ac040890969c14c9af
brianhhuynh38 <108171861+brianhhuynh38@users.noreply.github.com>
2023-12-24 14:09:04 -0500

    Completed homepage

--- Assets/Scripts/Utilities/SceneHandlers/MainMenu/MainMenu.cs
+++ Assets/Scripts/Utilities/SceneHandlers/MainMenu/MainMenu.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+///   Handles the timelines that are played and any necessary scene loading in the Homepage scene
+/// </summary>
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    /// <summary>The playable directors active in the scene</summary>
+    public List<PlayableDirector> directors;
+    /// <summary>Asynchronous operation to load the gallery scene</summary>
+    AsyncOperation _galleryAsyncLoad;
+
+    /// <summary>
+    ///   Plays the transitional cutscene to the Gallery scene while asynchronously loading the scene
+    /// </summary>
+    public void PlayEnterTimeline() {
+        // Start asynchronously loading the gallery scene while the cutscene plays
+        StartCoroutine(LoadGallery());
+        directors[1].Play();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// <summary>
+    ///   A couroutine to be used while the timeline is playing in order to speed up transitions
+    ///   when changing scenes.
+    /// </summary>
+    IEnumerator LoadGallery() {
+        // Try to load scene until the cutscene allows
+        yield return null;
+        _galleryAsyncLoad = SceneManager.LoadSceneAsync("Gallery");
+        _galleryAsyncLoad.allowSceneActivation = false;
+    }
+
+    /// <summary>
+    ///   Allows the scene activation to occur for the gallery scene
+    /// </summary>
+    public void AllowLoadGallery() {
+        _galleryAsyncLoad.allowSceneActivation = true;
     }
 }
